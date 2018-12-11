5d4740421f989ae2ce54155b6489472549edcedb
Lochan-Vishwanath <33321391+Lochan-Vishwanath@users.noreply.github.com>
2018-12-11 23:33:47 +0530

    Collision warning prototype

---
+++ C# College Project/Assets/Scripts/dangerobj.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dangerobj : MonoBehaviour {
+
+    public RectTransform panel;
+    public bool Show_GameOver_screen;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "enemy")
+        {
+            Destroy(collision.gameObject);
+            if(Show_GameOver_screen)
+                panel.gameObject.SetActive(true);
+        }
+    }
+}
