2f7b0435bda6334d77ef906afd8212f270a7a792
Lochan-Vishwanath <33321391+Lochan-Vishwanath@users.noreply.github.com>
2019-02-02 20:09:38 +0530

    10 Levels Demo

--- C# College Project/Assets/Scripts/SpikeMovement.cs
+++ C# College Project/Assets/Scripts/SpikeMovement.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class SpikeMovement : MonoBehaviour
 {
     public Transform destination1,destination2;
-    public float speed;
+    public float speed=0.07f;
     Transform destination;
+    public RectTransform panel;
     void Start()
     {
         destination = destination1;
     }
     void Update()
     {
+        //Debug.Log(Time.deltaTime*speed);
+        transform.position = Vector3.MoveTowards(transform.position, destination.position, speed);
         if (Vector2.Distance(transform.position, destination.position) == 0)
         {
             if (destination.Equals(destination1))
@@ -20,6 +23,15 @@ public class SpikeMovement : MonoBehaviour
             else
                 destination = destination1;
         }
-        transform.position = Vector3.MoveTowards(transform.position, destination.position,Time.deltaTime * speed);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "EnemyA")
+        {
+            Destroy(collision.gameObject);
+            panel.gameObject.SetActive(true);
+            AudioManager.playaudiogameover = true;
+        }
     }
 }
