6ce792f1ad9461195fedbcec32dcaeab6555d8f8
Lochan-Vishwanath <33321391+Lochan-Vishwanath@users.noreply.github.com>
2019-07-25 21:45:50 +0530

    Level info ui changed.

--- C# College Project/Assets/Scripts/lvlinfo.cs
+++ C# College Project/Assets/Scripts/lvlinfo.cs
@@ -14,8 +14,29 @@ public class lvlinfo : MonoBehaviour
         thisscene = SceneManager.GetActiveScene();
         char[] SceneName = thisscene.name.ToCharArray();
         //mytxt.text = "Level " + SceneName[1] + " Stage " + SceneName[3];
-        mytxt.text = "Level " + SceneName[1]+SceneName[2] + " Stage " + SceneName[4]+SceneName[5];
-        StartCoroutine(Pause(2));
+        //mytxt.text = "Level " + SceneName[1]+SceneName[2] + " Stage " + SceneName[4]+SceneName[5];
+        
+        List<string> scenesInBuild = new List<string>();
+        for (int i = 1; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+            int lastSlash = scenePath.LastIndexOf("/");
+            scenesInBuild.Add(scenePath.Substring(lastSlash + 1, scenePath.LastIndexOf(".") - lastSlash - 1));
+        }
+        for(int j=0;j<scenesInBuild.Count;j++)
+        Debug.Log(scenesInBuild[j]);
+        if (scenesInBuild.Contains(SceneName[0].ToString() + SceneName[1].ToString() + SceneName[2].ToString() +SceneName[3].ToString()+ 10))
+            //Debug.Log("here1");
+        mytxt.text = "Stage " + SceneName[4] + SceneName[5] + "/" + 10;
+        else if (scenesInBuild.Contains(SceneName[0].ToString() + SceneName[1].ToString() + SceneName[2].ToString()+SceneName[3].ToString()+0.ToString() + 7.ToString()))
+            //Debug.Log("here2");
+        mytxt.text = "Stage " + SceneName[4] + SceneName[5] + "/" + 0.ToString() + 7.ToString();
+        else if (scenesInBuild.Contains(SceneName[0].ToString() + SceneName[1].ToString() + SceneName[2].ToString() + SceneName[3].ToString() + 0.ToString() + 5.ToString()))
+            //Debug.Log("here3");
+        mytxt.text = "Stage " + SceneName[4] + SceneName[5] + "/" + 0.ToString() + 5.ToString();
+        else
+            Debug.Log("false");
+        StartCoroutine(Pause(3));
     }
     private IEnumerator Pause(int p)
     {
